41ebf3a7ef752ba91d0be3503e412fe309a57c28
Dylan Bussone <dylan.bussone@socrata.com>
2015-02-17 14:04:09 -0800

    Fix for neighboring like tags leaking markdown text

--- html2markdown.js
+++ html2markdown.js
@@ -228,7 +228,11 @@ function html2markdown(html, opts) {
 			case "dfn":
 			case "var":
 			case "cite":
-				nodeList.push(markdownTags[tag]);
+				if (nodeList[nodeList.length-1] === markdownTags[tag]) {
+					nodeList.splice(nodeList.length-1, 1);
+				} else {
+					nodeList.push(markdownTags[tag]);
+				}
 				break;
 			case "code":
 			case "span":
